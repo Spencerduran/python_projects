8b119175f1f090a1c01730758eefe50da39cb8e9
SpenceDuran <124907053+SpenceDuran@users.noreply.github.com>
2023-04-05 14:02:59 -0400

    window switcher

--- autotrader/order_sender.py
+++ autotrader/order_sender.py
@@ -1,20 +1,26 @@
 import discord
-import json
 import asyncio
 import datetime
 from pynput.keyboard import Controller, Key
+import PyGetwindow as gw
 
-DISCORD_TOKEN = "ODk2NDIyOTg1MTc3NjUzMzEw.GHEHKK.LjBUlyMpxp1UNnbBK87yoKviM5SSK85e02OeMI"
+DISCORD_TOKEN = "your_discord_token"
 WEBHOOK_USERNAME = "NQ Alert"
 CHANNEL_NAME = "chart-triggers"
 
 intents = discord.Intents.default()
-intents.message_content = True  # explicitly enable the message content intents
+intents.message_content = True
 intents.messages = True
 
 client = discord.Client(intents=intents)
 keyboard = Controller()
 
+def switch_to_window(target_window_title):
+    try:
+        target_window = gw.getWindowsWithTitle(target_window_title)[0]
+        target_window.activate()
+    except IndexError:
+        print(f"No window with the title '{target_window_title}' was found.")
 
 async def send_key_combination(keys):
     for key in keys:
@@ -23,26 +29,21 @@ async def send_key_combination(keys):
     for key in keys:
         keyboard.release(key)
 
-
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
-
 
 @client.event
 async def on_message(message):
     if message.author.name == WEBHOOK_USERNAME and message.channel.name == CHANNEL_NAME:
         now = datetime.datetime.now()
         print(message.content)
+        
+        # Switch to the specified window
+        switch_to_window("Chart - NQ 06-23")
+        
         content = message.embeds[0].title.strip().lower(
         ) if message.embeds else message.content.strip().lower()
-
-        if len(message.embeds) > 0:
-            content = message.embeds[0].title.strip().lower(
-            ) if message.embeds else message.content.strip().lower()
-
-        #print( f"Karma Algo: {message.content if message.content else content}")
-
         if "open long position alert" in content:
             await send_key_combination([Key.alt, Key.shift, "c"])
             print(f'\n{now}: Signal Bot: "Closed any open positions, Enter long"\n')
@@ -53,17 +54,17 @@ async def on_message(message):
         elif "open short position alert" in content:
             await send_key_combination([Key.alt, Key.shift, "c"])
             print(f'\n{now}: Signal Bot: "Closed any open position, Entering short"\n')
-            bot_message = "Closed any open positions, Entering short"
+            bot_message = "Closed any open positions, Entry short"
             await message.channel.send(bot_message)
             await send_key_combination([Key.alt, Key.shift, "s"])
         elif "close" in content:
             print(f'\n{now}: Signal Bot: "Closed position, all out"\n')
-            bot_message = "Closed position"
+            bot_message = "Closed position, all out"
             await message.channel.send(bot_message)
             await send_key_combination([Key.alt, Key.shift, "c"])
         elif "profit" in content:
             print(f'\n{now}: Signal Bot: "Taking Profit, all out"\n')
-            bot_message = "Profits bagged"
+            bot_message = "Bag secured"
             await message.channel.send(bot_message)
             await send_key_combination([Key.alt, Key.shift, "c"])
 
