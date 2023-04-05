70bb627bd3b66879fbbf725116eac123e1f0432b
SpenceDuran <124907053+SpenceDuran@users.noreply.github.com>
2023-04-05 19:10:17 -0400

    async window switcher

--- autotrader/order_sender.py
+++ autotrader/order_sender.py
@@ -1,12 +1,19 @@
-import discord
 import asyncio
 import datetime
+import ctypes
+from ctypes import wintypes
 from pynput.keyboard import Controller, Key
-from pywinauto import Desktop
+
+import discord
 
 DISCORD_TOKEN = "your_discord_token"
-WEBHOOK_USERNAME = "NQ Alert"
 CHANNEL_NAME = "chart-triggers"
+author_window_map = {
+    "ES Alert": "Chart - ES 06-23",
+    "NQ Alert": "Chart - NQ 06-23",
+    "YM Alert": "Chart - YM 06-23",
+    "RTY Alert": "Chart - RTY 06-23",
+}
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -15,14 +22,23 @@ intents.messages = True
 client = discord.Client(intents=intents)
 keyboard = Controller()
 
+# Define necessary Windows API functions and constants
+FindWindow = ctypes.windll.user32.FindWindowW
+FindWindow.argtypes = [wintypes.LPCWSTR, wintypes.LPCWSTR]
+FindWindow.restype = wintypes.HWND
+
+SetForegroundWindow = ctypes.windll.user32.SetForegroundWindow
+SetForegroundWindow.argtypes = [wintypes.HWND]
+SetForegroundWindow.restype = wintypes.BOOL
+
+
 def switch_to_window(target_window_title):
-    desktop_windows = Desktop(backend="uia").windows()
-    for window in desktop_windows:
-        if target_window_title == window.window_text():
-            window.set_focus()
-            break
+    hwnd = FindWindow(None, target_window_title)
+    if hwnd:
+        SetForegroundWindow(hwnd)
     else:
         print(f"No window with the title '{target_window_title}' was found.")
+
 
 async def send_key_combination(keys):
     for key in keys:
@@ -31,39 +47,44 @@ async def send_key_combination(keys):
     for key in keys:
         keyboard.release(key)
 
+
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
 
+
 @client.event
 async def on_message(message):
-    if message.author.name == WEBHOOK_USERNAME and message.channel.name == CHANNEL_NAME:
+    if message.channel.name == CHANNEL_NAME and message.author.name in author_window_map:
         now = datetime.datetime.now()
         print(message.content)
-        
-        # Switch to the specified window
-        switch_to_window("Chart - NQ 06-23")
-        
+
+        # Switch to the corresponding window for the message author
+        switch_to_window(author_window_map[message.author.name])
+
         content = message.embeds[0].title.strip().lower(
         ) if message.embeds else message.content.strip().lower()
         if "open long position alert" in content:
             await send_key_combination([Key.alt, Key.shift, "c"])
-            print(f'\n{now}: Signal Bot: "Closed any open positions, Enter long"\n')
-            bot_message = "Closed any open positions, Entering long"
+            print(f'\n{now}: Signal Bot: "Long Entry"\n')
+            bot_message = "Closed any open positions, Entered long"
             await message.channel.send(bot_message)
             await send_key_combination([Key.alt, Key.shift, "b"])
 
         elif "open short position alert" in content:
             await send_key_combination([Key.alt, Key.shift, "c"])
-            print(f'\n{now}: Signal Bot: "Closed any open position, Entering short"\n')
-            bot_message = "Closed any open positions, Entry short"
+            print(
+                f'\n{now}: Signal Bot: "Short Entry"\n')
+            bot_message = "Closed any open positions, Entered short"
             await message.channel.send(bot_message)
             await send_key_combination([Key.alt, Key.shift, "s"])
+
         elif "close" in content:
-            print(f'\n{now}: Signal Bot: "Closed position, all out"\n')
+            print(f'\n{now}: Signal Bot: "Flattened position, all out"\n')
             bot_message = "Closed position, all out"
             await message.channel.send(bot_message)
             await send_key_combination([Key.alt, Key.shift, "c"])
+
         elif "profit" in content:
             print(f'\n{now}: Signal Bot: "Taking Profit, all out"\n')
             bot_message = "Bag secured"
